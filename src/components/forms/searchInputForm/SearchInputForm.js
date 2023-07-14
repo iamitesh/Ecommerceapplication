5190e0f05c71abbe4ea8c510e50efd93a0889e53
Amitesh Anand <aamitesh7@gmail.com>
2023-07-14 06:51:20 +0530

    search feature is missing

--- src/components/forms/searchInputForm/SearchInputForm.js
+++ src/components/forms/searchInputForm/SearchInputForm.js
@@ -2,10 +2,13 @@ import React from "react";
 import "./searchinputform.styles.css";
 
 const SearchInputForm = () => {
+  const handleSearch=()=>{
+    //TODO
+  }
   return (
     <div className="search-input-form-container">
       <input type="text" className="search-input" placeholder="Search here" />
-      <button className="search-button"> Search </button>
+      <button className="search-button" onClick={handleSearch}> Search </button>
     </div>
   );
 };
